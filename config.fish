70b78c1ca6ff8634c7d47f1e67c4a883900d9f18
Arnaud <arnaud@talentpicker.io>
2024-11-11 23:09:52 +0100

    config.fish

---
+++ config.fish
@@ -0,0 +1,60 @@
+\if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fish_add_path /usr/local/bin
+
+function nvm
+    bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+end
+
+
+set -gx GPG_TTY (tty)
+
+## npm
+# alias i "npm install"
+# alias d "npm run dev"
+# alias t "npm run test"
+# alias sto "npm run storybook"
+# alias r "npm run"
+# alias sd "npm run start:dev"
+
+## git
+alias gp "git push"
+alias gc "git commit"
+alias ga "git add"
+alias gaa "git add -A"
+alias gaac "git add -A && git commit -m"
+alias gpl "git pull"
+alias gout "git checkout"
+alias gpp "git pull && git push"
+
+
+## pnpm
+alias p "pnpm"
+alias sto "pnpm storybook"
+alias d "pnpm run dev"
+alias px "pnpm dlx"
+alias b "pnpm run build"
+
+## misc
+alias remove "rm -rf"
+
+## fish
+alias conf "code ~/.config/fish/config.fish"
+
+## prisma
+alias studio "pnpm prisma studio"
+alias gen "pnpm prisma generate"
+alias dbp "pnpm prisma db push"
+
+## pnpm
+set -gx PNPM_HOME "/Users/arnaud/Library/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
+
+## python
+alias py "python3"
+
+## zed
+alias zed "open -a /Applications/Zed.app -n"
